9a1d175bc1b73cac92723d28a481769acf5272f2
Sam <stmiyamoto10@gmail.com>
2022-06-30 12:54:08 -0700

    Added comment with Python code for translation into JS

--- furtherStudy.js
+++ furtherStudy.js
@@ -1,5 +1,34 @@
 'use strict';
 
+// 
+/* 
+
+Translate the below Python code to JS: 
+
+def words_in_common(words1, words2):
+"""Return a list of unique words that appear in `words1` and `words2`
+
+Arguments:
+    words1 (list): A list of words
+    words2 (list): Another list of words
+
+Returns:
+    list: A list of unique words
+"""
+
+words1_set = set(words1)
+words2_set = set(words2)
+
+result = set()
+
+for word in words1_set:
+    if word in words2_set:
+        result.add(word)
+
+return list(result)
+
+*/
+
 function wordsInCommon(words1, words2) {
   // Replace this with your code
 }
